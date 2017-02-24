35abecc6473c6ace5f821fccf94865d71ca600c7
SMK-141510248 <141510248@smkassalaambandung.sch.id>
2017-02-24 20:25:23 +0700

    ini

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+	<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::resource('jabatans','JabatanController');
+Route::resource('golongans','GolonganController');
+Route::resource('kategori_lemburs','kategori_lemburController');
+Route::resource('pegawais','PegawaiController');
+Route::resource('lembur_pegawais','Lembur_pegawaiController');
+Route::resource('tunjangans','TunjangansController');
+Route::resource('tunjangan_pegawais','Tunjangan_pegawaiController');
+Route::resource('penggajians','PenggajianController');
+Route::resource('Admin','AdminController');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+/*Route::group(['middleware' => ['api'],'prefix' => 'api'], function () {
+*/
+
+Route::group(['middleware' => ['api']], function () {
+    Route::post('register', 'ApiController@register');
+    Route::post('login', 'ApiController@login');
+    Route::group(['middleware' => 'jwt-auth'], function () {
+    	Route::post('get_user_details', 'ApiController@get_user_details');
+    });
+});
